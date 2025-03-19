a5b60e7c8dffd5d72dead2d57cd28914f7c864e1
Anthony Astige <Anthony@Astige.com>
2025-03-19 09:26:11 -0500

    TODO

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export const rules = {
   "no-tsx-without-jsx": noTsxWithoutJsx,
   "max-tokens-per-file": maxTokensPerFile,
 };
-// TODO: Add recommended config
+// TODO: Add recommended config and use it in my repository instead of configing there
+// TODO: * Pull in all my config from repository and document it well in here
