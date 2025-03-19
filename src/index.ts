0325cd98f2f5136779ed4c8689d70a8326bd69bb
Anthony Astige <Anthony@Astige.com>
2025-03-19 16:32:35 -0500

    TODO's moves

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+// TODO: Pull in other eslint repositories I've made into here
+// TODO: * Updating their READMEs to point here, depublishing on NPM (is that good to do?), etc
+// TODO: * Remove all use from my repo, remove local instances of them, ...
+// TODO: * Add lint rule tests (figure out how to do it right)
+// TODO: Add recommended config and use it in my repository instead of configing there
+// TODO: * Pull in all my config from repository and document it well in here
+// TODO: Self-apply my full eslint system to this repository
 import { ftaComplexityCouldBeBetter, ftaComplexityNeedsImprovement } from "./rules/ftaComplexity";
 import { maxTokensPerFile } from "./rules/maxTokensPerFile";
 import { noImportAs } from "./rules/noImportAs";
@@ -40,13 +47,6 @@ export const rules = {
   "fta-complexity-could-be-better": ftaComplexityCouldBeBetter,
   "fta-complexity-needs-improvement": ftaComplexityNeedsImprovement,
 };
-// TODO: Pull in other eslint repositories I've made into here
-// TODO: * Updating their READMEs to point here, depublishing on NPM (is that good to do?), etc
-// TODO: * Remove all use from my repo, remove local instances of them, ...
-// TODO: * Add lint rule tests (figure out how to do it right)
-// TODO: Add recommended config and use it in my repository instead of configing there
-// TODO: * Pull in all my config from repository and document it well in here
-// TODO: Self-apply my full eslint system to this repository
 
 export const configs = {
   recommended,
