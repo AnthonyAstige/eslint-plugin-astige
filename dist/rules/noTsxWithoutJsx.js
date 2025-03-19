5761925651db651056caef6e210c5d4fb4ad45cc
Anthony Astige <Anthony@Astige.com>
2025-03-19 08:35:05 -0500

    npm run build

---
+++ dist/rules/noTsxWithoutJsx.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const rule = {
+    defaultOptions: [],
+    meta: {
+        type: "problem",
+        docs: {
+            description: "Disallow .tsx files without JSX",
+        },
+        messages: {
+            noJsxInTsx: "This file has a .tsx extension but does not contain any JSX elements.",
+        },
+        schema: [],
+    },
+    create(context) {
+        const filename = context.filename;
+        if (!filename.endsWith(".tsx")) {
+            return {};
+        }
+        let containsJSX = false;
+        return {
+            JSXElement() {
+                containsJSX = true;
+            },
+            JSXFragment() {
+                containsJSX = true;
+            },
+            "Program:exit"(node) {
+                if (!containsJSX) {
+                    const sourceCode = context.sourceCode;
+                    const firstToken = sourceCode.getFirstToken(node);
+                    if (firstToken) {
+                        context.report({
+                            loc: firstToken.loc,
+                            messageId: "noJsxInTsx",
+                        });
+                    }
+                }
+            },
+        };
+    },
+};
+exports.default = rule;
