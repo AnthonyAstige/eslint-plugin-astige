321884f66225b22ed81b6475c5006d3df42024fe
Anthony Astige <Anthony@Astige.com>
2025-03-19 16:37:16 -0500

    My main repo is updates to use this already now

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 // TODO: Pull in other eslint repositories I've made into here
 // TODO: * Updating their READMEs to point here, depublishing on NPM (is that good to do?), etc
-// TODO: * Remove all use from my repo, remove local instances of them, ...
 // TODO: * Add lint rule tests (figure out how to do it right)
 // TODO: Add recommended config and use it in my repository instead of configing there
 // TODO: * Pull in all my config from repository and document it well in here
