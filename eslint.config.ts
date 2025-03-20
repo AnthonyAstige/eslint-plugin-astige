9e497a07d511df45125bbe79d07b96ee721945db
Anthony Astige <Anthony@Astige.com>
2025-03-19 22:13:46 -0500

    No defineConfig

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,6 @@
 import typescriptParser from "@typescript-eslint/parser";
 import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { globalIgnores } from "eslint/config";
 import astige from "./dist/index.js";
 
 const config: FlatConfig.Config[] = [
