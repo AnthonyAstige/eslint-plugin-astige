3edbf05f6eaeb20ed4d2dc04fecad846b579d6ce
Anthony Astige <Anthony@Astige.com>
2025-03-19 22:13:31 -0500

    TODO

--- eslint.config.ts
+++ eslint.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import astige from "./dist/index.js";
 
 const config: FlatConfig.Config[] = [
+  // TODO: See if I can get away from languageOptions? Maybe once canonical auto is in
   {
     languageOptions: {
       parser: typescriptParser,
