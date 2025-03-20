aeb660ac96ba1714dd33f2255eb8a04162e88581
Anthony Astige <Anthony@Astige.com>
2025-03-20 18:37:46 -0500

    Naw on cleanup other repos ... just moving fast

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // TODO: Pull in other eslint repositories I've made into here
-// TODO: * Updating their READMEs to point here, depublishing on NPM (is that good to do?), etc
 // TODO: * Add lint rule tests (figure out how to do it right)
 // TODO: Add recommended config and use it in my repository instead of configing there
 // TODO: * Pull in all my config from repository and document it well in here
