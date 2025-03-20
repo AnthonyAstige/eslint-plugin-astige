704f319e496cf315fb95ddca6db4d5263404497f
Anthony Astige <Anthony@Astige.com>
2025-03-20 09:52:27 -0500

    TODO Done documenting & splitting

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import { astigeEveryConfig, astigeEveryPlugin } from "./plugins/astige-every";
 import { astigeIgnoreConfig } from "./plugins/astige-ignore";
 import { astigeJavascriptConfig, astigeJavascriptPlugin } from "./plugins/astige-javascript";
-// TODO: Update docs that this is a collection of plugins in a single plugin
-// TODO: * Maybe rename this to plugins?
-// TODO: * Explain why: Single repository, but allows splitting of plugin loading and processing per file type etc (like plugins do)
-// TODO: Split up this file and get organized
 // TODO: Pull in other eslint repositories I've made into here
 // TODO: * Updating their READMEs to point here, depublishing on NPM (is that good to do?), etc
 // TODO: * Add lint rule tests (figure out how to do it right)
