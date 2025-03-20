464cf037cd6a65f6c0c166d0c5f5ead3ac3e73b9
Anthony Astige <Anthony@Astige.com>
2025-03-19 19:57:21 -0500

    Split out javascript plugin

--- src/index.ts
+++ src/index.ts
@@ -8,49 +8,9 @@
 // TODO: Add recommended config and use it in my repository instead of configing there
 // TODO: * Pull in all my config from repository and document it well in here
 // TODO: Self-apply my full eslint system to this repository
-import type { FlatConfig, SharedConfig } from "@typescript-eslint/utils/ts-eslint";
-import { SEVERITY } from "./constants";
+import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
 import { everyConfig } from "./plugins/astige-every";
-import {
-  ftaComplexityCouldBeBetter,
-  ftaComplexityNeedsImprovement,
-} from "./plugins/astige-javascript/rules/ftaComplexity/ftaComplexity";
-import { noImportAs } from "./plugins/astige-javascript/rules/noImportAs/noImportAs";
-import { noTsxWithoutJsx } from "./plugins/astige-javascript/rules/noTsxWithoutJsx/noTsxWithoutJsx";
-
-const PLUGIN_NAME_JAVASCRIPT = "astige-javascript";
-
-const javascriptRules = {
-  "no-tsx-without-jsx": noTsxWithoutJsx,
-  "no-import-as": noImportAs,
-  "fta-complexity-could-be-better": ftaComplexityCouldBeBetter,
-  "fta-complexity-needs-improvement": ftaComplexityNeedsImprovement,
-};
-type PrefixedJavascriptRuleName = `${typeof PLUGIN_NAME_JAVASCRIPT}/${keyof typeof javascriptRules}`;
-const javascriptRuleConfigs: JavascriptRuleEntryObject = {
-  "astige-javascript/fta-complexity-could-be-better": [
-    SEVERITY.WARN,
-    { "when-above": 55, "when-at-or-under": 75 },
-  ] as const,
-  "astige-javascript/fta-complexity-needs-improvement": [
-    SEVERITY.ERROR,
-    { "when-above": 75 },
-  ] as const,
-  "astige-javascript/no-import-as": SEVERITY.ERROR,
-  "astige-javascript/no-tsx-without-jsx": SEVERITY.ERROR,
-};
-
-// TODO: Put rule types in here somehow from the actual rules so we config them right?
-type JavascriptRuleEntryObject = { [K in PrefixedJavascriptRuleName]: SharedConfig.RuleEntry };
-const javascriptConfig: {
-  files: FlatConfig.Config["files"];
-  plugins: { [PLUGIN_NAME_JAVASCRIPT]: FlatConfig.Plugin };
-  rules: JavascriptRuleEntryObject;
-} = {
-  files: ["**/*.{js,ts,jsx,tsx}"],
-  plugins: { [PLUGIN_NAME_JAVASCRIPT]: { rules: javascriptRules } },
-  rules: javascriptRuleConfigs,
-} as const;
+import { javascriptConfig } from "./plugins/astige-javascript";
 
 const configs = {
   recommended: javascriptConfig,
---
+++ src/plugins/astige-javascript/index.ts
@@ -0,0 +1,38 @@
+const PLUGIN_NAME_JAVASCRIPT = "astige-javascript";
+import type { FlatConfig, SharedConfig } from "@typescript-eslint/utils/ts-eslint";
+import { SEVERITY } from "../../constants";
+import { ftaComplexityCouldBeBetter, ftaComplexityNeedsImprovement } from "./rules/ftaComplexity/ftaComplexity";
+import { noImportAs } from "./rules/noImportAs/noImportAs";
+import { noTsxWithoutJsx } from "./rules/noTsxWithoutJsx/noTsxWithoutJsx";
+
+// TODO: Put rule types in here somehow from the actual rules so we config them right?
+// TODO: * Apply to all rules?
+type JavascriptRuleEntryObject = { [K in PrefixedJavascriptRuleName]: SharedConfig.RuleEntry };
+const javascriptRules = {
+  "no-tsx-without-jsx": noTsxWithoutJsx,
+  "no-import-as": noImportAs,
+  "fta-complexity-could-be-better": ftaComplexityCouldBeBetter,
+  "fta-complexity-needs-improvement": ftaComplexityNeedsImprovement,
+};
+type PrefixedJavascriptRuleName = `${typeof PLUGIN_NAME_JAVASCRIPT}/${keyof typeof javascriptRules}`;
+const javascriptRuleConfigs: JavascriptRuleEntryObject = {
+  "astige-javascript/fta-complexity-could-be-better": [
+    SEVERITY.WARN,
+    { "when-above": 55, "when-at-or-under": 75 },
+  ] as const,
+  "astige-javascript/fta-complexity-needs-improvement": [
+    SEVERITY.ERROR,
+    { "when-above": 75 },
+  ] as const,
+  "astige-javascript/no-import-as": SEVERITY.ERROR,
+  "astige-javascript/no-tsx-without-jsx": SEVERITY.ERROR,
+};
+export const javascriptConfig: {
+  files: FlatConfig.Config["files"];
+  plugins: { [PLUGIN_NAME_JAVASCRIPT]: FlatConfig.Plugin };
+  rules: JavascriptRuleEntryObject;
+} = {
+  files: ["**/*.{js,ts,jsx,tsx}"],
+  plugins: { [PLUGIN_NAME_JAVASCRIPT]: { rules: javascriptRules } },
+  rules: javascriptRuleConfigs,
+} as const;
