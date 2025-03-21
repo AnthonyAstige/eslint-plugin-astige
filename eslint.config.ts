ea0411b9bc74144fbb9580550e32950cae32f5bf
Anthony Astige <Anthony@Astige.com>
2025-03-21 11:03:25 -0500

    Ignore main config from main linting in this project for now

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,18 +1,19 @@
 import typescriptParser from "@typescript-eslint/parser";
-import type { FlatConfig } from "@typescript-eslint/utils/ts-eslint";
+import { type FlatConfig } from "@typescript-eslint/utils/ts-eslint";
 // import auto from "eslint-config-canonical/configurations/auto";
 import { globalIgnores } from "eslint/config";
-import * as astige from "./src/index";
+import * as astige from "./src";
 
 const configs: FlatConfig.Config[] = [
   // TODO: get away from languageOptions once canonical auto is in
   {
     languageOptions: {
       parser: typescriptParser,
-    },
+    }
   },
   // Custom ignores for this specific project
   globalIgnores([
+    "eslint.config.ts", // TODO 1. Fix so this file is linted too ; was trouble making work with typescriptParser (because this isn't under src which is our main tsconfig bundling)
     "dist",
     "docs/generated-create-eslint-plugin-instructions.md",
   ]),
