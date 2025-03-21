524566a6f86b1642edfe70b2587b5cf761942d6a
Anthony Astige <Anthony@Astige.com>
2025-03-21 15:45:40 -0500

    TODO 3 docs cleanup more

--- src/createRule.ts
+++ src/createRule.ts
@@ -1,8 +1,9 @@
 import { ESLintUtils } from "@typescript-eslint/utils";
 
 const createRule = ESLintUtils.RuleCreator(
-  // TODO 1. Move to rule specific documentation urls
-  // TODO 1. * Something like https://example.com/rule/${name}
+  // TODO 3. Move to rule specific documentation urls
+  // TODO 3. * Something like https://example.com/rule/${name}
+  // TODO 3. * Will need to move away from plugin level docs to rule level docs for this
   (_name) => `https://github.com/AnthonyAstige/eslint-plugin-astige`,
 );
 
