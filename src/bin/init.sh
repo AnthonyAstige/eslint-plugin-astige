d85a2dae12d70a7d509bc914168985a7bd6eba65
Anthony Astige <Anthony@Astige.com>
2025-03-22 08:02:57 -0500

    Init install dprint

--- src/bin/init.sh
+++ src/bin/init.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
-echo "INIT BIN SCRIPT CALLED C"
+
+DPRINT_FILE="./dprint.json"
+
+if [ -f "$DPRINT_FILE" ]; then
+  echo "Error: $DPRINT_FILE already exists in the application root!" >&2
+  exit 1
+fi
+
+echo "Creating $DPRINT_FILE in application root..."
+
+cat <<EOF >"$DPRINT_FILE"
+{
+  "extends": "./node_modules/eslint-plugin-astige/dist/dprint.json"
+}
+EOF
+
+echo "$DPRINT_FILE created successfully!"
